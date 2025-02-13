65727f4952322186d6263f174dc93d102f7dfb86
JanSolecki1 <dartanian99@wp.pl>
2025-02-14 00:00:37 +0100

    version 10

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
-import Recipes from './pages/Recipes'; 
+import Recipes from './pages/Recipes';
 import Favorites from './pages/Favorites';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -19,6 +19,8 @@ function App() {
           <Route path="/favorites" element={<Favorites />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          {/* Catch-all route */}
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </main>
       <Footer />
